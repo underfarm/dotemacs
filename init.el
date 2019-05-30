1c5ee5b30acfef3779a6609e36d9fdf84fef3f9c
Ulrik Farmen <ulrik.bruun.farmen@gmail.com>
2019-05-30 12:32:28 +0200

    Made org mode more pretty

--- init.el
+++ init.el
@@ -199,6 +199,14 @@
   ;; (add-hook 'powershell-mode-hook #'lsp)
   )
 
+;; We need to add git to path, if not then magit will complain.
+(setq ubf|additional-exec-paths '(
+			     "c:/Program Files/git/bin"
+			     ))
+
+(setenv "PATH" (concat (getenv "PATH") (mapconcat 'identity ubf|additional-exec-paths ";")))
+(setq exec-path (append exec-path ubf|additional-exec-paths))
+
 (use-package smex)
 
 (use-package magit)
@@ -317,11 +325,54 @@
   (org-mode . org-indent-mode)
 
   :config
+
+  ;; Make it pretty
+  ;;https://zzamboni.org/post/beautifying-org-mode-in-emacs/
+
+(custom-theme-set-faces
+ 'user
+ '(org-block                 ((t (:inherit fixed-pitch))))
+ '(org-document-info         ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-link                  ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line             ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value        ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword       ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-tag                   ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim              ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent                ((t (:inherit (org-hide fixed-pitch))))))
+
+  (font-lock-add-keywords 'org-mode
+                        '(("^ *\\([-]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(let* ((variable-tuple
+        (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+              ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+              ((x-list-fonts "Verdana")         '(:font "Verdana"))
+              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+       (base-font-color     (face-foreground 'default nil 'default))
+       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+  (custom-theme-set-faces
+   'user
+   `(org-level-8 ((t (,@headline ,@variable-tuple))))
+   `(org-level-7 ((t (,@headline ,@variable-tuple))))
+   `(org-level-6 ((t (,@headline ,@variable-tuple))))
+   `(org-level-5 ((t (,@headline ,@variable-tuple))))
+   `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+   `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+   `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+   `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+   `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
   (require 'ox-org)
   (require 'ox-html)
 
   (setq org-startup-indented t
 	org-log-done 'time
+	org-hide-emphasis-markers t
 	org-pretty-entities t
 	org-blank-before-new-entry t
 	org-hide-leading-stars t
@@ -596,33 +647,6 @@ holding export options."
   (if(equal (buffer-name) "*eshell*")
       (switch-to-buffer (other-buffer (current-buffer) 1))
     (eshell)))
-
-;; (ubf--eshell-paths '("*eshell*"))
-
-(defun ubf--eshell-paths (buffer-names)
-  (let* ((paths (mapcar (lambda (x)
-			  (cons x (with-current-buffer x
-			    (pwd))))
-			buffer-names)))
-    (cl-loop for p in paths
-	     collect (cons (car p) (last (s-split "/" (cdr p) t))))))
-
-(defun ubf|eshell-generate ()
-  (interactive)
-  (let* ((buffs (mapcar 'buffer-name (buffer-list)))
-	 (members (remove nil (mapcar '(lambda (x)
-			   (when (s-contains? "eshell" x) x))
-			  buffs)))
-	 (current-name (buffer-name)))
-    (if (-contains? (ubf--eshell-paths members)  (car (last (s-split "/" (file-name-directory (buffer-file-name)) t))))
-	(message "CONTAINS!!!!"))))
-	
-
-		 )))
-
-  (car (last (s-split "/" (file-name-directory (buffer-file-name)) t)))
-
-
 
 (setq eshell-prompt-function
   	    (lambda ()
